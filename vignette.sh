a267fb4c64ea36c673303c2f669dc950024d3706
Speykious <speykious@gmail.com>
2021-10-11 21:35:47 +0200

    Shortened state_home variable assignment

--- vignette.sh
+++ vignette.sh
@@ -24,7 +24,7 @@ install_dir=/usr/lib/vignette
 cubism=CubismSdkForNative-4-r.3
 libcubism=libLive2DCubismCore.so
 
-[ -f "$XDG_STATE_HOME" ] && state_home="$XDG_STATE_HOME" || state_home=~/.local/state
+state_home="${XDG_STATE_HOME-~/.local/state}"
 
 if [ ! -f "$install_dir/$libcubism" ] && [ ! -f "$state_home/vignette/$libcubism" ]; then
   zenity --question --text="It seems like you don't have the Cubism SDK installed on your system, or locally for Vignette.\\nDo you accept the <a href=\"https://www.live2d.com/en/download/cubism-sdk/download-native/\">Live2D Proprietary Software License Agreement</a>?" --width=480 || exit 1
